2a48a12ee5a35801cd335f9c4613bddad8c504aa
BotsClub <BotsClub@users.noreply.github.com>
2022-02-26 11:07:51 +0000

    Automated code formatting
    Signed-off-by: BotsClub <BotsClub@users.noreply.github.com>

--- Avenger/modules/disable.py
+++ Avenger/modules/disable.py
@@ -312,7 +312,6 @@ if is_module_loaded(FILENAME):
     def __chat_settings__(chat_id, user_id):
         return build_curr_disabled(chat_id)
 
-
     __help__ = """
 /cmds*:* check the current status of disabled commands
 *Admins only:*
